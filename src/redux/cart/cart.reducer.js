3a6c192251decf82e9aefc2a0ef56c342be9f7c8
LeonSoares <leonsoares3@gmail.com>
2021-04-01 19:47:55 -0400

    add addItem and removeItem functionality to checkout item arrows

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import {CartActionTypes} from './cart.types';
-import { addItemToCart } from './cart.utils.js'
+import { addItemToCart, removeItemFromCart } from './cart.utils.js'
 // this is when our app first get initiated/ default value
 const INITIAL_STATE = {
     hidden: true,
@@ -18,6 +18,16 @@ const INITIAL_STATE = {
         return{
             ...state,
             cartItems: addItemToCart(state.cartItems, action.payload)
+        }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+        return{
+          ...state,
+          cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+        };
+        case CartActionTypes.REMOVE_ITEM:
+        return{
+          ...state,
+          cartItems: removeItemFromCart(state.cartItems, action.payload)
         }
       default:
         return state;
