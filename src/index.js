598f67c17900b6a1ea0fbe9cf2c90d4e0d00514a
LeonSoares <leonsoares3@gmail.com>
2021-04-02 14:13:06 -0400

    implement data persistence for cart items with redux-persist

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import {store, persistor} from './redux/store'
 
 
 import App from './App';
@@ -12,9 +14,11 @@ import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
         <App />
-      </BrowserRouter>
+        </PersistGate>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,38 @@
 import { combineReducers } from 'redux'
+
+import {persistReducer} from 'redux-persist'
+// importing the type os store we want ( localStorage or sessionStorage)
+// sessionStorage would be a different path/
+import storage from 'redux-persist/lib/storage'
+
+
 import userReducer from './user/user-reducer'
 import cartReducer from './cart/cart.reducer'
 
+
+// redux persist config
+const persistConfig = {
+    key: 'root',
+    storage,
+
+    // pass here the reducers name that we want to store
+    whitelist: ['cart'] // we are not passing 'user' because it is been handled by firebase
+}
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    cart: cartReducer
+})
+
 // this is our combined reducer/ our root reducer
+export default persistReducer(persistConfig, rootReducer)
+
+/* THIS IS WHAT WE EXPORTED BEFORE PERSISTREDUCER
+this is our combined reducer/ our root reducer
+
 export default combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+*/
