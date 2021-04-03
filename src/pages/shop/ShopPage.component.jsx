a293ab0f959a1902f4b7527cb8c9067dde8713a8
LeonSoares <leonsoares3@gmail.com>
2021-04-03 13:25:37 -0400

    move directory and shop data into its own reducer. Create respective selectors and update corresponding components with redux flow for directory/shop

--- src/pages/shop/ShopPage.component.jsx
+++ src/pages/shop/ShopPage.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './shopPage.styles.scss'
 import CollectionsOverview from '../../components/collections-overview/Collections-overview.component'
+import { Route } from 'react-router-dom'
+import CollectionPage from '../collection/Collection.component'
 
 
 import CollectionPreview from '../../components/collection-preview/Collection-preview.component'
 
-const ShopPage = () => (
+const ShopPage = ({match}) => (
     <div className="shop-page">
-            <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>       
 )
 
